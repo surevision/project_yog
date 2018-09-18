c0a183620e78d6a5fde7973cfaf6f20b6c53ce27
surevision <surevision@163.com>
2018-09-18 18:10:25 +0800

    355执行脚本

--- Assets/scripts/game/GameInterpreter.cs
+++ Assets/scripts/game/GameInterpreter.cs
@@ -283,7 +283,7 @@ public class GameInterpreter {
                     return this.command_wait();
                 case CommandTypes.EvalScript:   // 355 脚本
                     Debug.Log(string.Format("CommandTypes.EvalScript", this.currentParam));
-                    //return this.command_evalScript();
+                    return this.command_evalScript();
                     return true;
                 default:
                     return true;
@@ -419,6 +419,35 @@ public class GameInterpreter {
     /// </summary>
     public bool command_wait() {
         this.waitCount = int.Parse(this.currentParam[0]);
+        return true;
+    }
+
+
+    /// <summary>
+    ///  355 eval
+    ///  lua脚本
+    /// </summary>
+    public bool command_evalScript() {
+        string src = this.currentParam[0];
+
+        Debug.Log(src);
+
+        XLua.LuaTable scriptEnv = LuaManager.LuaEnv.NewTable();
+        // 为每个脚本设置一个独立的环境，可一定程度上防止脚本间全局变量、函数冲突
+        XLua.LuaTable meta = LuaManager.LuaEnv.NewTable();
+        meta.Set("__index", LuaManager.LuaEnv.Global);
+        scriptEnv.SetMetaTable(meta);
+        meta.Dispose();
+
+        scriptEnv.Set("self", this);
+        scriptEnv.Set("gameVariables", GameTemp.gameVariables);
+        scriptEnv.Set("gameSwitches", GameTemp.gameSwitches);
+        scriptEnv.Set("gameSelfSwitches", GameTemp.gameSelfSwitches);
+        scriptEnv.Set("gamePlayer", GameTemp.gamePlayer);
+        scriptEnv.Set("gameMap", GameTemp.gameMap);
+
+        LuaManager.LuaEnv.DoString(src, string.Format("event_eval_{0}", this.eventId), scriptEnv);
+
         return true;
     }
 
