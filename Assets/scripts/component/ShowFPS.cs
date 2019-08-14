6124f1750f65cec6adeb316bb562f417f99b64ed
surevision <surevision@163.com>
2019-08-14 19:55:04 +0800

    追逐战优化

--- Assets/scripts/component/ShowFPS.cs
+++ Assets/scripts/component/ShowFPS.cs
@@ -4,32 +4,12 @@ using UnityEngine;
 
 public class ShowFPS : MonoBehaviour
 {
-	//更新的时间间隔
-	public float UpdateInterval = 1f;
-	//最后的时间间隔
-	private float _lastInterval;
-	//帧[中间变量 辅助]
-	private int _frames = 0;
-	//当前的帧率
-	private float _fps;
 	void Start()
 	{
 	    //Application.targetFrameRate=60;
-	    UpdateInterval = Time.realtimeSinceStartup;
-	    _frames = 0;
 	}
 	void OnGUI()
 	{
-	    GUI.Label(new Rect(20, 20, 200, 200), "FPS:" + _fps.ToString("f2"));
-	}
-	void Update()
-	{
-	    ++_frames;
-	    if (Time.realtimeSinceStartup > _lastInterval + UpdateInterval)
-        {
-            _fps = _frames / (Time.realtimeSinceStartup - _lastInterval);
-            _frames = 0;
-            _lastInterval = Time.realtimeSinceStartup;
-        }
+		GUI.Label(new Rect(0, 0, 100, 100), (1.0f / Time.smoothDeltaTime).ToString("f2"));
 	}
 }
