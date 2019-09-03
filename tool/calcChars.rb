0e795cf967f92a4adfa2a5103db866383259f736
surevision <surevision@163.com>
2019-09-03 14:03:44 +0800

    文本工具

--- tool/calcChars.rb
+++ tool/calcChars.rb
@@ -2,12 +2,33 @@
 require "find"
 require "fileutils"
 
-result = {}
+result = {}	# 认为对话只有一行
 Find.find("../Assets/resources/prefabs/maps/") {|filename|
 	p filename
 	next if File.directory?(filename)
 	File.open(filename, "rb"){|file|
 		file.each_line{|line|
+			line.force_encoding('utf-8')
+			eval("\"#{$1}\"").split(//).each{|c|result[c] = true} if line[/"(.*)?"$/]
+		}
+	}
+}
+Find.find("../Assets/resources/data/") {|filename|
+	p filename
+	next if File.directory?(filename)
+	File.open(filename, "rb"){|file|
+		file.each_line{|line|
+			line.force_encoding('utf-8')
+			line.split(//).each{|c|result[c] = true}
+		}
+	}
+}
+Find.find("../Assets/resources/prefabs/ui/menus/") {|filename|
+	p filename
+	next if File.directory?(filename)
+	File.open(filename, "rb"){|file|
+		file.each_line{|line|
+			line.force_encoding('utf-8')
 			eval("\"#{$1}\"").split(//).each{|c|result[c] = true} if line[/"(.*)?"$/]
 		}
 	}
